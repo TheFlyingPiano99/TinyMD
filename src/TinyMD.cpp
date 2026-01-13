5f0b89ad1fee174bee6df5901292bd2f2d9a9e5c
TheFlyingPiano99 <simonzolika1999@gmail.com>
2026-01-13 14:15:34 +0100

    Added CSV export

--- src/TinyMD.cpp
+++ src/TinyMD.cpp
@@ -1,6 +1,7 @@
 #include "TinyMD.h"
 #include <print>
 #include <format>
+#include <fstream>
 
 namespace tinymd {
 
@@ -15,7 +16,7 @@ namespace tinymd {
 
     template<tinyla::RealType T>
     void MDSimulator<T>::run() {
-        for (uint32_t step = 0; step < m_step_count; ++step) {
+        for (m_current_step = 0; m_current_step < m_step_count; ++m_current_step) {
 
             // Reset accelerations:
             for (auto& atom : m_atoms) {
@@ -72,17 +73,23 @@ namespace tinymd {
 
             // Debug print:
             if (m_print_debug) {
-                std::println("Step {}\nNo. of atoms = {}", step, m_atoms.size());
-                for (const auto& atom : m_atoms) {
-                    std::println("Atom ID: {}, r = {}, r' = {}, r'' = {}", atom.get_id(), to_string(atom.get_position()), to_string(atom.get_velocity()), to_string(atom.get_acceleration()));
-                }
+                std::println("Step {}\nNo. of atoms = {}", m_current_step, m_atoms.size());
+                print_atom_states();
+            }
+            bool is_export_csv = true;
+            if (is_export_csv && !m_export_path.empty()) {
+                export_csv(m_export_path, m_current_step > 0);
             }
         }
     }
 
     template<tinyla::RealType T>
     void MDSimulator<T>::interact_atoms(Atom<T>& atom1, Atom<T>& atom2) {
-        // Coulomb interaction
+        coulomb_interaction(atom1, atom2);
+    }
+
+    template<tinyla::RealType T>
+    void MDSimulator<T>::coulomb_interaction(Atom<T>& atom1, Atom<T>& atom2) {
         constexpr T k_e = static_cast<T>(1.0); // Coulomb constant in Hartree atomic units
         
         // Calculate distance vector from atom2 to atom1
@@ -111,5 +118,42 @@ namespace tinymd {
         atom2.apply_force(-force, m_delta_time);
     }
 
+    template<tinyla::RealType T>
+    void MDSimulator<T>::print_atom_states() const {
+        for (const auto& atom : m_atoms) {
+            std::println("Atom ID: {}, r = {}, r' = {}, r'' = {}", atom.get_id(), to_string(atom.get_position()), to_string(atom.get_velocity()), to_string(atom.get_acceleration()));
+        }
+    }
 
-} // namespace TinyMD
+    template<tinyla::RealType T>
+    void MDSimulator<T>::export_csv(const std::string& filename, bool append) const {
+        std::ofstream file(filename, append ? std::ios_base::app : std::ios_base::out);
+        
+        if (!file.is_open()) {
+            throw std::runtime_error(std::format("Failed to open file for writing: {}", filename));
+        }
+
+        if (!append) {
+            // Write header
+            file << "position_x,position_y,position_z,rotation_x,rotation_y,rotation_z,atom_id,simulation_step\n";
+        }
+
+        // Write atom positions
+        for (const auto& atom : m_atoms) {
+            auto pos = atom.get_position();
+            auto rot = atom.get_rotation();
+            file << std::format("{},{},{},{},{},{},{},{}\n", 
+                static_cast<double>(pos.eval_at(0, 0)),
+                static_cast<double>(pos.eval_at(1, 0)),
+                static_cast<double>(pos.eval_at(2, 0)),
+                static_cast<double>(rot.eval_at(0, 0)),
+                static_cast<double>(rot.eval_at(1, 0)),
+                static_cast<double>(rot.eval_at(2, 0)),
+                atom.get_id(),
+                m_current_step
+            );
+        }
+        file.close();
+    }
+
+} // namespace tinymd
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "TinyMD.h"
 #include <print>
+#include <filesystem>
 
 int main(int argc, char** argv) {
     std::println("Launching TinyMD Simulator...");
@@ -14,20 +15,28 @@ int main(int argc, char** argv) {
     };
     simulator.add_atom(std::move(atom0));
     auto atom1 = tinymd::Atom<double>{
-        tinyla::dvec3{10.0, 1.0, 1.0},
+        tinyla::dvec3{5.0, 1.0, 1.0},
         tinyla::dvec3{0.0, 0.0, 0.0},
         1.0,
         5.0
     };
     simulator.add_atom(std::move(atom1));
     auto atom2 = tinymd::Atom<double>{
-        tinyla::dvec3{20.0, 1.0, 1.0},
+        tinyla::dvec3{10.0, 1.0, 1.0},
         tinyla::dvec3{0.0, 0.0, 0.0},
         1.0,
         5.0
     };
     simulator.add_atom(std::move(atom2));
 
+    simulator.set_time_step(0.1);
+    simulator.set_step_count(1000);
+
+    // Export atom positions to CSV for visualization in Blender
+    auto output_dir = std::filesystem::path("E:/coding/CppProjects/TinyMD/output");
+    std::filesystem::create_directories(output_dir);
+    auto csv_path = output_dir / "atom_positions.csv";
+    simulator.set_export_path(csv_path.string());
     // Run simulation:
     simulator.run();
 
